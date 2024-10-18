4b0bf6c95e1a37aa0968c4d467c39e0a05626fc4
Aniket jauhri <aniketjauhri2003@gmail.com>
2024-10-18 17:06:37 +0530

    done

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use(
 );
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true }));
-app.use(express.cookieParser());
+app.use(cookieParser());
 
 // Routes
 app.use("/api/users", userRoutes);
