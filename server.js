a2d50afd5e1c9bf4d5a86db6c61af89160ec8c63
Aniket jauhri <aniketjauhri2003@gmail.com>
2024-10-18 12:52:21 +0530

    done

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ console.log(process.env.CLOUDINARY_CLOUD_NAME);
 // Middlewares
 app.use(
   cors({
-    origin: "https://thread-clone-sr1c.vercel.app", 
+    origin: "https://thread-frontend-inky.vercel.app/auth", 
     credentials: true, 
   })
 );
